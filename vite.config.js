4efe7062e8d5f3e5f31718e5fcc4993ca8e54ce0
Akash <akash200307@outlook.com>
2024-12-08 22:58:08 +0530

    Dockerfile changed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }  ,
   build: {
     outDir: 'dist',
     sourcemap: true
